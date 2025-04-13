8e686ea5ea57ce4b80d31958879282d032266729
Ryan Jasinski <jasinski.17@gmail.com>
2025-04-13 07:34:42 -0700

    folders ref

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@
 # Note these paths are relative to the script location
 
 # Where are we puling your new music from?
-SOURCE_FOLDER = "/Volumes/Rekordbox SSD/_Unsorted"
+SOURCE_FOLDER = ".new-music-input"
 
 # Where are we putting your music sorted by GENERE?
-GENRE_FOLDER = "/Volumes/Rekordbox SSD/Organized"
+GENRE_FOLDER = ".sorted-music-output.Genre"
 
 # Where are we putting your music sorted by ENERGY?
-ENERGY_FOLDER = "/Volumes/Rekordbox SSD/Energy"
+ENERGY_FOLDER = ".sorted-music-output.Energy"
